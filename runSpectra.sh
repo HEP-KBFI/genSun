e2b0126cc585b68f754b4a4ceb89d2f1ec482bba
jpata <joosep.pata@gmail.com>
2013-01-30 01:35:32 +0200

    switch to cardSunBatch, 5Tev

--- runSpectra.sh
+++ runSpectra.sh
@@ -1,4 +1,4 @@
-DMMASS=1000
+DMMASS=5000
 
 OF="out_m_"$DMMASS
 mkdir $OF
@@ -6,13 +6,13 @@ particles=( 1 5 6 11 12 13 15 22 23 24 25  )
 for i in "${particles[@]}"
 do
 	#Hadrons probabilistic, light hadrons stop, ch. leptons probabilistic
-	time ./genSun.exe $i $DMMASS $OF/output_prob$i.root gammaPlusX.card 2 1 2 &> $OF/log$i & 
+	./genSun.exe $i $DMMASS $OF/output_prob$i.root cardSunBatch.card 2 1 2 &> $OF/log_prob$i & 
 	
 	#heavy hadrons average, light hadrons stop, ch. leptons average 
-        time ./genSun.exe $i $DMMASS $OF/output_avg$i.root gammaPlusX.card 1 1 1 &> $OF/log$i & 
+        ./genSun.exe $i $DMMASS $OF/output_avg$i.root cardSunBatch.card 1 1 1 &> $OF/log_avg$i & 
 
 	#no energy loss
-	time ./genSun.exe $i $DMMASS $OF/output_no$i.root gammaPlusX.card 0 0 0 &> $OF/log$i & 
+	./genSun.exe $i $DMMASS $OF/output_no$i.root cardSunBatch.card 0 0 0 &> $OF/log_no$i & 
 done
 
 
