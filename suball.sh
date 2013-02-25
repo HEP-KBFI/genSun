f6a12fcfcaf683d0d54326f579d8a1099b072a0c
jpata <joosep.pata@gmail.com>
2013-02-25 14:06:55 +0200

    submit a few

--- suball.sh
+++ suball.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #masses=( 1000 2500 5000 7500 10000 )
 masses=( 5000 )
-particles=( 1 5 6 11 12 13 15 22 23 24 25 )
+#particles=( 1 5 6 11 12 13 15 22 23 24 25 )
+particles=( 6 15 )
 OFDIR=/home/joosep/solarNu/out
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for m in "${masses[@]}"
@@ -9,12 +10,12 @@ for m in "${masses[@]}"
         for p in "${particles[@]}"
             do
                 qsub $DIR/runSolarSpectrum.sh $p $m 2 1 2 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 1 1 1 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 1 1 1 $OFDIR
                 qsub $DIR/runSolarSpectrum.sh $p $m 0 0 0 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 1 0 0 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 0 1 0 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 1 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 2 0 0 $OFDIR
-                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 2 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 1 0 0 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 0 1 0 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 1 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 2 0 0 $OFDIR
+#                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 2 $OFDIR
             done
     done
