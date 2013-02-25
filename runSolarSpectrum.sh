126abb927fa51854b693be39b45316f3a788468f
jpata <joosep.pata@gmail.com>
2013-02-25 20:43:50 +0200

    cmssw env

--- runSolarSpectrum.sh
+++ runSolarSpectrum.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 uname -a
 WD=/scratch/joosep/solarNuWD-$SLURM_JOB_ID
-source /cvmfs/cms.cern.ch/slc5_amd64_gcc472/lcg/root/5.34.04/bin/thisroot.sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-
+cd ~/CMSSW_5_3_8_patch3
+scramv1 runtime -sh
+source /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms21/bin/thisroot.sh
+export PYTHIA8DATA=/home/joosep/solarNu/pythia8175/xmldoc/
 PARTID=$1
 DMMASS=$2
 HHADINSTR=$3
@@ -17,14 +19,11 @@ echo "HHADINSTR="$HHADINSTR
 echo "LHADINSTR="$LHADINSTR
 echo "LEPINSTR="$LEPINSTR
 
-ROOTSYS=/home/joosep/root
-#PATH=/home/joosep/local/include:/home/joosep/local/bin:/home/joosep/local/lib:$ROOTSYS/lib:$PATH
-#LD_LIBRARY_PATH=/home/joosep/local/lib:$LD_LIBRARY_PATH
-source ~/root/bin/thisroot.sh
-cp -R /home/joosep/solarNu/pythia8170 $WD
+cp -R /home/joosep/solarNu/pythia8175 $WD
 cd $WD
 cd genSun
-LD_LIBRARY_PATH=/home/joosep/local/lib:/home/joosep/local/lib64:$LD_LIBRARY_PATH ./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR &> log \
+#LD_LIBRARY_PATH=/home/joosep/local/lib:/home/joosep/local/lib64:$LD_LIBRARY_PATH ./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR &> log \
+ ./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR &> log \
 && cp output.root $OFDIR/output_$SLURM_JOB_ID.root
 mv log $OFDIR/log_$SLURM_JOB_ID.txt
 
