956c82686c51510d651c3a2e823cb10844f0ac90
jpata <joosep.pata@gmail.com>
2013-03-22 13:12:44 +0200

    added scripts to hadd files on cluster

---
+++ parhadd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/joosep/CMSSW_5_3_8_patch3
+eval `scramv1 runtime -sh`
+source /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms21/bin/thisroot.sh
+cd $SLURM_SUBMIT_DIR
+OUTNAME="out_"$SLURM_JOB_ID".root"
+hadd $OUTNAME $@
---
+++ subhadd.sh
@@ -0,0 +1,1 @@
+parallel -n 100 sbatch parhadd.sh -- \ls -1 /home/joosep/solarNu/out/*.root
