4a692bc6de93a21a44c52db7266d024ea3899b2f
jpata <joosep.pata@gmail.com>
2013-03-22 14:07:18 +0200

    keep only full mc energy loss

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd ~/CMSSW_5_3_8_patch3
 eval `scramv1 runtime -sh`
-source /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms21/bin/thisroot.sh
+#source /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms21/bin/thisroot.sh
 export PYTHIA8DATA=/home/joosep/solarNu/pythia8175/xmldoc/
 cd /home/joosep/solarNu
