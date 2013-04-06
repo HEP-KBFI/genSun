eb4e3139fd600905ad36a4da5d13dabcfe46cd15
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-04-06 03:21:02 +0300

    Add default implementations for statistics base.

--- geant/src/include/StatisticsRunAction.hh
+++ geant/src/include/StatisticsRunAction.hh
@@ -7,7 +7,8 @@ class G4Run;
 
 class StatisticsRunActionTarget {
 	public:
-		virtual void onBeginRun(const G4Run* run)=0;
+		virtual void onBeginRun(const G4Run* run){};
+		virtual void onEndRun(const G4Run* run){};
 };
 
 class StatisticsRunAction : public G4UserRunAction {
@@ -16,7 +17,7 @@ class StatisticsRunAction : public G4UserRunAction {
 	public:
 		//G4Run* GenerateRun();
 		void BeginOfRunAction(const G4Run* run);
-		//void EndOfRunAction(const G4Run*);
+		void EndOfRunAction(const G4Run*);
 		
 		void addTarget(StatisticsRunActionTarget* target);
 };
