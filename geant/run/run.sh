0673c27a56ccb0bac22eb21e2ed1ea7149654bcd
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-04-08 01:24:53 +0300

    Add datetime to run.sh

--- geant/run/run.sh
+++ geant/run/run.sh
@@ -6,8 +6,9 @@ energy=$3
 runs=$4
 
 ofname=working/${prefix}/${prefix}_${SLURM_PROCID}.root
-echo "Saving to: " ${ofname}
 
+echo "Run ${SLURM_PROCID} start:" `date` " -- fout:" ${ofname}
 time ./../build/solnugeant -q -n${runs} --ofile=${ofname} ${particle} ${energy}
+echo "Run {SLURM PROCID} stop:" `date`
 
 #echo procid:$SLURM_PROCID localid:$SLURM_LOCALID
