71610b4ac0548e09e3675d8a84f9bc6f73d813fa
jpata <joosep.pata@gmail.com>
2013-04-17 16:13:57 +0300

    running

--- suball.sh
+++ suball.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #masses=( 1000 2500 5000 7500 10000 )
-masses=( 10 100 1000 10000 100000 )
-#masses=( 10000 )
-particles=( 1 2 3 4 5 6 11 12 13 15 22 23 24 25 )
-#particles=( 1 2 3 4 5 6 )
-#particles=( 6 15 )
+#masses=( 10 100 1000 10000 100000 )
+masses=( 10000 )
+#particles=( 1 4 5 6 11 12 13 15 21 22 23 24 25 )
+particles=( 13 ) 
 OFDIR=/home/joosep/solarNu/out
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for m in "${masses[@]}"
@@ -13,7 +12,7 @@ for m in "${masses[@]}"
             do
                 sbatch $DIR/runSolarSpectrum.sh $p $m 2 1 2 $OFDIR
 #                qsub $DIR/runSolarSpectrum.sh $p $m 1 1 1 $OFDIR
-#                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 0 $OFDIR
+                sbatch $DIR/runSolarSpectrum.sh $p $m 0 0 0 $OFDIR
 #                qsub $DIR/runSolarSpectrum.sh $p $m 1 0 0 $OFDIR
 #                qsub $DIR/runSolarSpectrum.sh $p $m 0 1 0 $OFDIR
 #                qsub $DIR/runSolarSpectrum.sh $p $m 0 0 1 $OFDIR
--- subhadd.sh
+++ subhadd.sh
@@ -1,1 +1,1 @@
-parallel -n 100 sbatch parhadd.sh -- \ls -1 /home/joosep/solarNu/out/*.root
+parallel -n 100 sbatch parhadd.sh -- `\ls -1 /home/joosep/solarNu/out/*.root`
