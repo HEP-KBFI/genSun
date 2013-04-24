7cc51ed7f01dd498fe6a8556b957e0d56728716a
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-04-24 17:09:58 +0300

    Update 1000GeV gamma job.

--- geant/run/sendjobs.sh
+++ geant/run/sendjobs.sh
@@ -4,7 +4,7 @@
 sbatch -n100   batch.sh -r100000 22 1      # 20min/10000 runs
 sbatch -n100   batch.sh -r10000  22 10
 sbatch -n50    batch.sh -r2000   22 100    # ~10s per run; 100k events
-sbatch -n50    batch.sh -r2000   22 1000   # ~1.5m per run; 100k events
+sbatch -n100    batch.sh -r1000   22 1000   # ~1.5m per run; 100k events
 sbatch -n2000  batch.sh -r50     22 10000  # ~15m per run; 100k events
 
 # Z bosons
