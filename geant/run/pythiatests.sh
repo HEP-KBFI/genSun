95561b1466ad4ea93471a07e96b67c15c6a22594
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-04-25 09:58:31 +0300

    Add W boson test.

--- geant/run/pythiatests.sh
+++ geant/run/pythiatests.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Z bosons
-sbatch -n100 batch.sh -pTRANS -r100000 23 1   # 10M events
-#sbatch -n100 batch.sh -pTRANS -r100000 23 10   # 10M events
-#sbatch -n100 batch.sh -pTRANS -r100000 23 100   # 10M events
-sbatch -n100 batch.sh -pTRANS -r100000 23 1000   # 10M events
-#sbatch -n100 batch.sh -pTRANS -r100000 23 10000   # 10M events
-sbatch -n100 batch.sh -pTRANS -r100000 23 100000   # 10M events
+# Z boson
+sbatch -n100 batch.sh -pVAC -r100000 23 1   # 10M events
+sbatch -n100 batch.sh -pVAC -r100000 23 1000   # 10M events
+sbatch -n100 batch.sh -pVAC -r100000 23 100000   # 10M events
+
+# W boson
+sbatch -n100 batch.sh -pVAC -r100000 24 1000 # 10M events
+sbatch -n100 batch.sh -pVAC -r100000 24 100000 # 10M events
+
+# H boson
+sbatch -n100 batch.sh -pVAC -r50000 25 100000   # 5M events
+
+# top quark
+sbatch -n100 batch.sh -pVAC -r10000 6 100000   # 1M events
