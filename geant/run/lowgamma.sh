d7881820535ca4862d1eff128848ed6bcd94706a
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-05 02:09:35 +0300

    Fix lowgamma.sh

--- geant/run/lowgamma.sh
+++ geant/run/lowgamma.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Low energy gammas
-sbatch -n100 -uM  batch.sh -r1000000 22 1      # ~ 2 sec / 1000 runs
-sbatch -n100 -uM  batch.sh -r1000000 22 10     # ~ 2 sec / 1000 runs
-sbatch -n100 -uM  batch.sh -r100000  22 100    # ~10 sec / 1000 runs
-sbatch -n100 -uM  batch.sh -r10000   22 1000   # ~10 sec /  100 runs
+sbatch -n100 batch.sh -r1000000 22 1 -uM     # ~ 2 sec / 1000 runs
+sbatch -n100 batch.sh -r1000000 22 10 -uM    # ~ 2 sec / 1000 runs
+sbatch -n100 batch.sh -r100000  22 100 -uM   # ~10 sec / 1000 runs
+sbatch -n100 batch.sh -r10000   22 1000 -uM  # ~10 sec /  100 runs
 
