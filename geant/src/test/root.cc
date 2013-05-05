9a0a2e7c58f82e8cf409fcbe608ace26e5525006
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-05-05 23:33:17 +0300

    Create ROOT files with root.cc

--- geant/src/test/root.cc
+++ geant/src/test/root.cc
@@ -1,17 +1,35 @@
 #include <iostream>
+
 #include "TRandom.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TH1F.h"
 
 using namespace std;
 
 bool p_quiet = false;
 
-void test() {
-	TRandom *rnd = new TRandom(time(0));
-	Double_t x = rnd->Rndm();
-	cout << "x = " << x << endl;
-}
-
 int main() {
-	test();
+	TRandom *rnd = new TRandom(time(0));
+	
+	TH1F h1("unit", "", 20, 0, 1);
+	TH1F h2("real", "", 50, -5, 5);
+	//h.SetDirectory(&dir);
+	for(int i=0; i<10000; i++) {
+		h1.Fill(rnd->Rndm());
+		h2.Fill(rnd->Rndm()*2 - 0.5);
+	}
+	
+	TFile* tfile = new TFile("ofile.root", "RECREATE");
+	TDirectory* dir = tfile->mkdir("dir1")->mkdir("dir2");
+	dir->cd();
+	
+	h1.Write();
+	h2.Write();
+	
+	//tfile->cd();
+	tfile->ls();
+	tfile->Close();
+	
 	return 0;
 }
