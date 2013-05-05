be89301497929846b121ec795de673ffa30e7e1c
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-05 20:10:59 +0300

    Update lowgamma job definitions.

--- geant/run/lowgamma.sh
+++ geant/run/lowgamma.sh
@@ -4,5 +4,8 @@
 sbatch -n100 batch.sh -r1000000 22 1 -uM     # ~ 2 sec / 1000 runs
 sbatch -n100 batch.sh -r1000000 22 10 -uM    # ~ 2 sec / 1000 runs
 sbatch -n100 batch.sh -r100000  22 100 -uM   # ~10 sec / 1000 runs
-sbatch -n100 batch.sh -r10000   22 1000 -uM  # ~10 sec /  100 runs
+sbatch -n100 batch.sh -r100000  22 200 -uM   # ~20 sec / 1000 runs
+sbatch -n100 batch.sh -r100000  22 500 -uM   # ~ 6 sec /  100 runs
+sbatch -n100 batch.sh -r100000  22 800 -uM   # ~10 sec /  100 runs
+sbatch -n250 batch.sh -r40000   22 1000 -uM  # ~10 sec /  100 runs
 
