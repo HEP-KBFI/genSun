d126e142d2cee98942cc2473e8f3e2713313b6ec
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-12 23:15:08 +0300

    Create a run report script.

---
+++ geant/run/report.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+import glob, re
+from datetime import datetime
+
+dfm = '%a %b %d %H:%M:%S %Z %Y'
+
+slurmfiles = sorted(glob.glob('slurm-*'))
+
+for sf in slurmfiles:
+	#print '==========================   %s   =========================='%(sf)
+	fh = open(sf, 'r')
+	lines = fh.readlines()
+	lines = map(lambda x: x.strip(), lines)
+	
+	m_jobid = int(re.match('slurm-(.*).out', sf).group(1))
+	m_runs = int(re.match('runs=(.*)', lines[4]).group(1))
+	m_physics = re.match('physics=(.*)', lines[5]).group(1)
+	m_particle = re.match('particle=(.*)', lines[6]).group(1)
+	m_energy = re.match('energy=(.*)', lines[7]).group(1)
+	m_prefix = re.match('prefix=(.*)', lines[8]).group(1)
+	
+	date_start = datetime.strptime(lines[1], dfm)
+	try:
+		date_end = datetime.strptime(lines[-1], dfm)
+		delta = date_end - date_start
+		run_time = (date_end - date_start)/m_runs
+	except ValueError:
+		date_end = 'NOTFINISHED'
+		delta = 'NOTFINISHED'
+		run_time = 'NOTFINISHED'
+	
+	print 'J {0}: {1:>2} {2:>7} {3:>5} {4:>14} {5}'.format(m_jobid, m_particle, m_energy, m_physics, run_time, date_start)
+	#print 'Start/end: %s / %s'%(date_start, date_end)
+	#print 'Delta: %s (%s /run)'%(delta, run_time)
+	
+	fh.close()
