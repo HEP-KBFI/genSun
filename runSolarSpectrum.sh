aa0c15c32b58134027489ae21d52e2cd3a1f6e90
jpata <joosep.pata@gmail.com>
2013-05-14 15:34:42 +0300

    retry on failure

--- runSolarSpectrum.sh
+++ runSolarSpectrum.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 uname -a
+date
 WD=/scratch/joosep/solarNuWD-$SLURM_JOB_ID
 source /home/joosep/solarNu/setenv.sh
-
 PARTID=$1
 DMMASS=$2
 HHADINSTR=$3
@@ -19,8 +19,18 @@ echo "LEPINSTR="$LEPINSTR
 cp -R /home/joosep/solarNu/pythia8175 $WD
 cd $WD
 cd genSun
-./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR \
-&& cp output.root $OFDIR/output_$SLURM_JOB_ID.root
+#LD_LIBRARY_PATH=/home/joosep/local/lib:/home/joosep/local/lib64:$LD_LIBRARY_PATH ./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR &> log \
+for i in {1..50}
+do
+    echo "Trying to run genSun.exe, try "$i
+    srun ./genSun.exe $PARTID $DMMASS output.root cardSunBatch.card $HHADINSTR $LHADINSTR $LEPINSTR
+    if [ $? -eq 0 ]
+    then
+        break
+    fi
+done
+cp output.root $OFDIR/output_$SLURM_JOB_ID.root
+#mv log $OFDIR/log_$SLURM_JOB_ID.txt
 
 ls -al
 rm -Rf $WD
