2194aa09b7c5d7983b18ea22d9d460af45babf6b
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-20 02:29:18 +0300

    Update fulljobs.sh

--- geant/run/fulljobs.sh
+++ geant/run/fulljobs.sh
@@ -2,8 +2,8 @@
 
 # Gammas
 #sbatch -n100   batch.sh -r100000 22 1      # --
-sbatch -n100   batch.sh -r10000  22 10     # ~  2 sec / run (from: 0:15 / 10 runs)
-sbatch -n100   batch.sh -r1000   22 100    # ~ 12 sec / run (from: 1:50 / 10 runs)
+sbatch -n100   batch.sh -r100000  22 10     # ~ 0.02 sec / run (from: 23 sec / 1000 runs)
+sbatch -n100   batch.sh -r10000   22 100    # ~ 0.2  sec / run (from: 20 sec / 100 runs)
 #sbatch -n100   batch.sh -r1000   22 1000   # --
 #sbatch -n2000  batch.sh -r50     22 10000  # --
 
@@ -11,4 +11,4 @@ sbatch -n100   batch.sh -r1000   22 100    # ~ 12 sec / run (from: 1:50 / 10 run
 #sbatch -n200   batch.sh -r10000  23 1000   # --
 
 # Quarks - top
-#sbatch -n100   batch.sh -r10000  6 1000    # --
+sbatch -n100   batch.sh -r10000  6 1000     # ~ 2    sec / run (from: 200 sec / 100 runs)
