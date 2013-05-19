e4645210dcfe36d63c8a566dfba116045fe8cea7
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-19 20:28:49 +0300

    Update full jobs list.

--- geant/run/fulljobs.sh
+++ geant/run/fulljobs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Gammas
-sbatch -n100   batch.sh -r100000 22 1      # 20min/10000 runs
-sbatch -n100   batch.sh -r10000  22 10
-sbatch -n50    batch.sh -r2000   22 100    # ~10s per run; 100k events
-sbatch -n100   batch.sh -r1000   22 1000   # ~1.5m per run; 100k events
-#sbatch -n2000  batch.sh -r50     22 10000  # ~15m per run; 100k events
+#sbatch -n100   batch.sh -r100000 22 1      # --
+sbatch -n100   batch.sh -r10000  22 10     # ~  2 sec / run (from: 0:15 / 10 runs)
+sbatch -n100   batch.sh -r1000   22 100    # ~ 12 sec / run (from: 1:50 / 10 runs)
+#sbatch -n100   batch.sh -r1000   22 1000   # --
+#sbatch -n2000  batch.sh -r50     22 10000  # --
 
 # Z bosons
-sbatch -n200   batch.sh -r10000  23 1000   # check; 2M events
+#sbatch -n200   batch.sh -r10000  23 1000   # --
+
+# Quarks - top
+#sbatch -n100   batch.sh -r10000  6 1000    # --
