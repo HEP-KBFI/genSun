8d461a013f2f90afdff0e41ca94441b01fcd5edc
jpata <joosep.pata@gmail.com>
2013-05-24 11:34:19 +0300

    need to set pythia data explicitly

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,5 @@ export PATH=/home/joosep/gcc-4.8.0/bin:$PATH
 export LD_LIBRARY_PATH=/home/joosep/gcc-4.8.0/lib:/home/joosep/gcc-4.8.0/lib64:$LD_LIBRARY_PATH
 export INCLUDE_DIR=/home/joosep/gcc-4.8.0/include/
 export LIB_DIR=/home/joosep/gcc-4.8.0/include/
-#export PYTHIA8DATA=/home/joosep/solarNu/pythia8175/xmldoc/
+export PYTHIA8DATA=/home/joosep/solarNu/pythia8176/xmldoc/
 cd /home/joosep/solarNu
