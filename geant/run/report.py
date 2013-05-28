ddce016361629721ae3746ad2a0c059858c66542
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-28 21:59:38 +0300

    Update report.py

--- geant/run/report.py
+++ geant/run/report.py
@@ -27,7 +27,7 @@ for sf in slurmfiles:
 	def mfn(p,f,s):
 		m = re.search(p,s)
 		return f(m.group(1)) if m is not None else None
-	for ln in lines[:200]:
+	for ln in lines[:2000]:
 		rd=map(lambda(k,p,f): (k,mfn(p,f,ln)), ms_p)
 		ms += filter(lambda (k,s): s is not None, rd)
 		if len(set(map(lambda(k,v):k, ms)))==len(ms_p): break
@@ -46,11 +46,18 @@ for sf in slurmfiles:
 		delta = date_end - date_start
 		run_time = (date_end - date_start)/m_runs
 	except ValueError:
-		date_end = 'NOTFINISHED'
-		delta = '+'+str(datetime.now().replace(microsecond=0) - date_start)
-		run_time = 'NOTFINISHED'
+		cncls = map(lambda s: re.search('CANCELLED', s) is not None, lines[-20:])
+		cncl = reduce(lambda x,y: x or y, cncls)
+		if cncl:
+			date_end = 'CANCELLED'
+			delta = '???'
+			run_time = 'CANCELLED'
+		else:
+			date_end = 'NOTFINISHED'
+			delta = '+'+str(datetime.now().replace(microsecond=0) - date_start)
+			run_time = 'NOTFINISHED'
 	
-	print 'J {0}: {1:>4} {2:>7} {3:>5} {4:>10}  {5:>14}  {6}  {7}'.format(m_jobid, m_particle, m_energy, m_physics, delta, run_time, date_start, m_phystring)
+	print 'J {0} {1:>4} {2:>7} {3:>5} {4:>10}  {5:>14}  {6}  {7}'.format(m_jobid, m_particle, m_energy, m_physics, delta, run_time, date_start, m_phystring)
 	#print 'Start/end: %s / %s'%(date_start, date_end)
 	#print 'Delta: %s (%s /run)'%(delta, run_time)
 	
