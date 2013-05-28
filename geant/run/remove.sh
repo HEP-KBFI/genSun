ac6fb6a096354c6291c4a0adb0115754b8ce8126
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-05-28 20:11:41 +0300

    Allow for multiple jobids on remove.sh

--- geant/run/remove.sh
+++ geant/run/remove.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "Removing job" $1
-rm -v slurm-${1}.out
-rm -vR {ready,working}/hist_job${1}_*
+echo "Removing job(s)" $@
+for x in $@
+do
+	echo "Removing job" $x
+	rm -v slurm-${x}.out
+	rm -vR {ready,working}/hist_job${x}_*
+done
