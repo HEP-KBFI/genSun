42ebeb06cdce37da1809cf76c595cef82bc62a88
jpata <joosep.pata@gmail.com>
2013-05-28 09:22:08 +0300

    more parallel jobs

--- util/subhadd.sh
+++ util/subhadd.sh
@@ -1,3 +1,3 @@
 DIR=`readlink -f $1`
 #parallel -n 100 sbatch /home/joosep/solarNu/util/parhadd.sh -- `find $DIR -name "*.root"`
-find $DIR -name "*.root" -print0 | xargs -n400 -0 sbatch /home/joosep/solarNu/util/parhadd.sh
+find $DIR -name "out*.root" -print0 | xargs -n50 -0 sbatch /home/joosep/solarNu/util/parhadd.sh
