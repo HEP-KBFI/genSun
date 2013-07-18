672fd957be324afe56bf40168c26e9f352585e50
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-07-18 18:57:11 +0300

    Update analyze.py

--- geant/run/measure/analyze.py
+++ geant/run/measure/analyze.py
@@ -11,6 +11,14 @@ optruns = [
 	20000, 25000, 50000, 100000
 ]
 
+import argparse_local as argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--low', type=float, default=0)
+parser.add_argument('-u', '--high', type=float, default=1e5)
+parser.add_argument('-e', '--events', type=float, default=1e5)
+args = parser.parse_args()
+print 'Arguments:', args
+
 def findOptRuns(T):
 	fs = filter(lambda t: t>T, optruns)
 	return fs[0] if len(fs)>0 else optruns[-1]
@@ -33,13 +41,14 @@ o[24] = dict(filter(lambda (k,v): k >=  81.0, o[24].items()))
 o[25] = dict(filter(lambda (k,v): k >= 127.0, o[25].items()))
 
 def filter_fn((m,o)):
-	#return m <= 5000
-	return True
+	return m > args.low and m <= args.high
+	#return m in [5,10,50,100,500,1000,5000,10000,50000,100000]
 
 for ch in o:
 	o[ch] = dict(filter(filter_fn,  o[ch].items()))
 
-totNodes = 0
+totNodes = 0; totNodes_E = {}
+totJobs = 0; totJobs_E = {}
 for (ch,es) in o.items():
 	es = o[ch]
 	for (e,d) in sorted(es.items()):
@@ -47,18 +56,32 @@ for (ch,es) in o.items():
 		demi = 30*60/mean # number of events in half an hour
 		optDemi = findOptRuns(demi)
 		realTime = mean*optDemi/60
-		parallel = int(1e5/optDemi)
+		parallel = int(args.events/optDemi)
 		jobs,nodes = minimizeOpt(parallel)
 		print '{0:>2d} {1:>6d} {2:>10.4f} {3:>12.2f} \033[1m{4:>6d}\033[0m {5:>5.2f} {6:>6d} \033[1m{7:3d}\033[0m \033[1m{8:3d}\033[0m'.format(ch, e, mean, demi, optDemi, realTime, parallel, jobs, nodes)
 		
 		totNodes += parallel
+		if not e in totNodes_E:
+			totNodes_E[e] = 0
+		totNodes_E[e] += parallel
+		
+		totJobs += jobs
+		if not e in totJobs_E:
+			totJobs_E[e] = 0
+		totJobs_E[e] += jobs
+		
 		d['calc'] = {
 			'mean_time': mean, 'runs_per_30min': demi,
 			'rounded_runs_per_30min': optDemi, 'rounded_time': realTime,
 			'parallel_threads': parallel, 'jobs': jobs, 'nodes_per_job': nodes
 		}
 
-print totNodes, totNodes/5e3, (totNodes/5e3)/24
+print 'Node count distribution:', totNodes_E
+print 'Job count distribution:', totJobs_E
+
+print 'Total nodes (N, N/5k, N/clust.days):', totNodes, totNodes/5e3, (totNodes/5e3)/24
+print 'Total cluster jobs:', totJobs
+
 
 fhtml = open('report.html', 'w')
 print >>fhtml, '<html>'
@@ -74,7 +97,7 @@ for (ch,es) in o.items():
 		print >>fhtml, '<th>{0}</th>'.format(key)
 	print >>fhtml, '</tr>'
 	
-	totNodes = 0
+	chTotNodes = 0
 	for m,d in sorted(es.items()):
 		print >>fhtml, '<tr>'
 		print >>fhtml, '<td>{0}</td>'.format(ch)
@@ -83,9 +106,9 @@ for (ch,es) in o.items():
 			print >>fhtml, '<td>{0}</td>'.format(v)
 		print >>fhtml, '</tr>'
 		
-		totNodes += d['calc']['parallel_threads']
+		chTotNodes += d['calc']['parallel_threads']
 	
-	sumstring = 'Nodes needed for 100k: <b>{0}</b>'.format(totNodes)
+	sumstring = 'Nodes needed for 100k: <b>{0}</b>'.format(chTotNodes)
 	print >>fhtml, '<tr><td colspan="{0}"><i>{1}</i></td></tr>'.format(len(keys), sumstring)
 	
 	print >>fhtml, '</table>'
@@ -94,8 +117,13 @@ print >>fhtml, '</html>'
 fhtml.close()
 
 fscript = open('submitscript.sh', 'w')
-fscript.write('''#!/bin/bash
-
+fscript.write('#!/bin/bash\n\n')
+print >>fscript, '# Target events:', str(int(args.events/1e3))+'k'
+print >>fscript, '# Total nodes (N, N/5k, N/clust.days):', totNodes, totNodes/5e3, (totNodes/5e3)/24
+print >>fscript, '# Total cluster jobs:', totJobs
+print >>fscript, '# Node count distribution:', totNodes_E
+print >>fscript, '# Job count distribution:', totJobs_E
+fscript.write('''
 Tn=$1 # unit: 30-60min, 100k events
 Nn=1
 
