9f1a3c02f8f067125da01042f95a4ae60d839805
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-07-26 01:44:06 +0300

    Add a flag to batch for storing events.

--- geant/run/batch.sh
+++ geant/run/batch.sh
@@ -2,13 +2,14 @@
 #SBATCH -J "solnuG4" -x comp-c-[002,004,006,009-012,014,016,020,022,024,025,028,031,032,034,036,037],comp-d-[001,083,098,111,117]
 
 # Parse arguments
-GETOPT=$(getopt -n "batch" -o r:p:u:c: -l trk: -- "$@")
+GETOPT=$(getopt -n "batch" -o r:p:u:c: -l trk:,store -- "$@")
 runs=1 # default number of runs
 physics="FULL" # default physics
 units="G" # default GeV units
 creator="PYTHIA8"
 creator_suffix=""
 trk_arg=""
+store_events=false
 eval set -- "$GETOPT"
 while true; do
 	case $1 in
@@ -33,6 +34,10 @@ while true; do
 			trk_arg="--track-kill="$2
 			shift 2; continue
 		;;
+		--store)
+			store_events=true
+			shift 1; continue
+		;;
 		--)
 			break
 		;;
@@ -52,7 +57,14 @@ shift 2
 # Check for output directory
 if [ ! -d hists ]; then mkdir hists; fi
 
-prefix=hist_job${SLURM_JOB_ID}_p${particle}_e${energy}${units}_P${physics}${creator_suffix}
+job_identifier=job${SLURM_JOB_ID}_p${particle}_e${energy}${units}_P${physics}${creator_suffix}
+prefix="hist_"${job_identifier}
+
+store_arg=""
+if $store_events ; then
+	if [ ! -d events ]; then mkdir events; fi
+	store_arg="--store-events=events/events_"${job_identifier}.root
+fi
 
 echo "Starting batch job" ${SLURM_JOB_ID}
 date
@@ -76,7 +88,7 @@ echo "Run ${SLURM_PROCID} start:" `date`
 echo "arguments=$@"
 
 echo "Starting SolNuGeant (Geant4) simulation.."
-time ./../solnugeant -q --ofile=${ofname} --seed=$seed -n${runs} ${particle} ${energy} -u${units} -p${physics} -c${creator} ${trk_arg} $@
+time ./../solnugeant -q --ofile=${ofname} --seed=$seed -n${runs} ${particle} ${energy} -u${units} -p${physics} -c${creator} ${trk_arg} ${store_arg} $@
 echo "Geant simulation finished!"
 
 echo "Batch job" ${SLURM_JOB_ID} "done!"
