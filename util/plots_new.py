9b1b3aab6d8ff95bd08b7049d2aa0b64dab52084
Joosep Pata <joosep.pata@gmail.com>
2013-07-29 23:10:23 +0200

    various updates

--- util/plots_new.py
+++ util/plots_new.py
@@ -131,6 +131,8 @@ class EnergyLoss:
     def lossType(self):
         if self.h_had==2 and self.l_had==1 and self.ch_lep==2:
             return 1
+        elif self.h_had==1 and self.l_had==1 and self.ch_lep==1:
+            return 2
         elif self.h_had==0 and self.l_had==0 and self.ch_lep==0:
             return 0
         else:
@@ -153,6 +155,7 @@ class EnergyDistribution:
     def __init__(self, *args, **kwargs):
         self.hist = args[0]
 
+        self.card = kwargs["card"]
         self.dm_mass = float(kwargs["dm_mass"])
         self.decay_channel = kwargs["decay_channel"]
         self.n_events = float(kwargs["n_events"])
@@ -160,14 +163,18 @@ class EnergyDistribution:
         self.energy_loss = EnergyLoss(kwargs["h_had"], kwargs["l_had"], kwargs["ch_lep"])
 
         self.bin_width = 0.03
-        if self.fstate in ["nuel", "numu", "nutau"]:
-            mult = 0.5
-        else:
-            mult = 1.0
+
+        # Present in conv8.cc by Mario, not present in the the Fornengo simulation
+        # if self.fstate in ["nuel", "numu", "nutau"]:
+        #     mult = 0.5
+        # else:
+        #     mult = 1.0
+        mult = 1.0
+
         sf = mult/(self.n_events*self.bin_width)
         pre_scale = self.hist.Integral()
         self.hist.Scale(sf)
-        print "%s: pre-scale=%.3E, sf=%.3E, post-scale=%.3E" % (self, pre_scale, sf, self.hist.Integral())
+        #print "%s: pre-scale=%.3E, sf=%.3E, post-scale=%.3E" % (self, pre_scale, sf, self.hist.Integral())
         self.x = [x for x in self.hist.x()]
         self.y = [y for y in self.hist.y()]
 
@@ -177,7 +184,7 @@ class EnergyDistribution:
         n_bins = self.hist.GetNbinsX()
         lxtab = numpy.linspace(-9, 0, 300)
         tot = sum([0.03*(10**lxtab[i - 1])*self.hist.GetBinContent(i) for i in range (1,301)])
-        print "%s: %.2E" % (self, tot)
+        #print "%s: %.2E" % (self, tot)
         for n in range(1, n_bins):
             ostr += ("%.9f" % float(self.hist.GetBinContent(n))) + ","
         ostr += ("%.9f" % (self.hist.GetBinContent(n_bins) + self.hist.GetBinContent(n_bins+1))) + "};"
@@ -190,10 +197,16 @@ class EnergyDistribution:
         return self.__repr__()
 
 if __name__=="__main__":
-    f = rootpy.io.open("merged_Jun6.root")
-    #f = rootpy.io.open("output.root")
 
-    pat = re.compile("mass_([0-9]*)/particle_([0-9]*)/energyLoss_hhad_([0-9])_lhad_([0-9])_chlep_([0-9])")
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infile", help="the input root file", type=str, required=True)
+    parser.add_argument("--outdir", help="the output directory", type=str, required=True)
+    args = parser.parse_args()
+
+    f = rootpy.io.File(args.infile)
+
+    pat = re.compile("card_(.*)/mass_([0-9]*)/particle_([0-9]*)/energyLoss_hhad_([0-9])_lhad_([0-9])_chlep_([0-9])")
     interesting_hists = ["nuel", "numu", "nutau", "anuel", "anumu", "anutau"]#{"nu_electron": "nuel", "nu_muon": "numu", "nu_tau": "nutau", "gamma": "gam", "proton": "proton", "neutron": "neutron", "ele":"el"}
     hists = dict()
     energy_distributions = dict()
@@ -201,25 +214,41 @@ if __name__=="__main__":
     energy_distributions_part = dict()
 
 
-    for elem in f.walk():
-        m = pat.match(elem[0])
+    #Create .nb files for these particles
+    channels = [1, 4, 5, 6, 11, 13, 15, 21, 22, 23, 24, 25]
+
+    for root, dirs, items in f.walk():
+        m = pat.match(root)
         if m is not None:
-            mass = int(m.group(1))
-            partId = int(m.group(2))
-            hHadInstr = int(m.group(3))
-            lHadInstr = int(m.group(4))
-            chLepInstr = int(m.group(5))
+            card = m.group(1)
+            mass = int(m.group(2))
+            partId = int(m.group(3))
+            hHadInstr = int(m.group(4))
+            lHadInstr = int(m.group(5))
+            chLepInstr = int(m.group(6))
+
+            if not partId in channels:
+                continue
+            cardn = card.split(".")[0]
+            eloss = "%d_%d_%d" % (hHadInstr, lHadInstr, chLepInstr)
+            ofd = os.path.join(args.outdir, cardn, eloss)
+            try:
+                os.makedirs(ofd)
+            except:
+                pass
+
 
             for hname in interesting_hists:
-                name = elem[0] + "/" + hname
+                name = root + "/" + hname
                 hists[name] = f.get(name)
-                event_status_hist = f.get(elem[0] + "/eventStatus")
+                event_status_hist = f.get(root + "/eventStatus")
                 successful_events = float(event_status_hist.GetBinContent(1))
                 if successful_events>0.0:
                     hists[name].Sumw2()
 #                    hists[name].Scale(float(10**5)/float(successful_events))
                     energy_distributions[name] = EnergyDistribution(
                         hists[name],
+                        card=card,
                         dm_mass=mass,
                         decay_channel=partId,
                         h_had=hHadInstr,
@@ -231,36 +260,47 @@ if __name__=="__main__":
                     if partId not in energy_distributions_part.keys():
                         energy_distributions_part[partId] = []
                     energy_distributions_part[partId].append(energy_distributions[name])
+                    
+                    chan_name = decay_channel_names_Mathematica[partId]
+                    fn = "DM%s.nb" % chan_name
+                    ofn = os.path.join(ofd, fn)
+                    ofi = open(ofn, "a")
+                    ofi.write(energy_distributions[name].printArray() + "\n")
+                    ofi.close()
         else:
             continue
 
-    #Create .nb files for these particles
-    channels = [1, 4, 5, 6, 11, 13, 15, 21, 22, 23, 24, 25]
 
-    timestamp = strftime("%m_%d", gmtime())
-    dir_no_loss = "spec_loss_off_%s"%timestamp
-    dir_with_loss = "spec_loss_on_%s"%timestamp
-    os.mkdir(dir_no_loss)
-    os.mkdir(dir_with_loss)
+    # timestamp = strftime("%m_%d", gmtime())
+    # dir_no_loss = args.outdir + "/spec_loss_off_%s"%timestamp
+    # dir_with_loss = args.outdir + "/spec_loss_on_%s"%timestamp
+    # dir_with_avg_loss = args.outdir + "/spec_loss_on_avg_%s"%timestamp
+    # os.makedirs(dir_no_loss)
+    # os.makedirs(dir_with_loss)
+    # os.makedirs(dir_with_avg_loss)
 
-    for (part, distrs) in energy_distributions_part.items():
-        if part not in channels:
-            continue
-        chan_name = decay_channel_names_Mathematica[part]
-        #chan_name.replace("\\", r'\\\')
+    # for (part, distrs) in energy_distributions_part.items():
+    #     if part not in channels:
+    #         continue
+    #     chan_name = decay_channel_names_Mathematica[part]
+    #     #chan_name.replace("\\", r'\\\')
 
 
-        ofname_no_loss = dir_no_loss + "/DM%s.nb" % chan_name
-        ofname_with_loss = dir_with_loss + "/DM%s.nb" % chan_name
-        ofile_no_loss = open(ofname_no_loss, "w")
-        ofile_with_loss = open(ofname_with_loss, "w")
-        for dist in distrs:
-            if dist.energy_loss.lossType()==0:
-                ofile_no_loss.write(dist.printArray() + "\n")
-            elif dist.energy_loss.lossType()==1:
-                ofile_with_loss.write(dist.printArray() + "\n")
-        ofile_no_loss.close()
-        ofile_with_loss.close()
+    #     ofname_no_loss = dir_no_loss + "/DM%s.nb" % chan_name
+    #     ofname_with_loss = dir_with_loss + "/DM%s.nb" % chan_name
+    #     ofname_with_avg_loss = dir_with_avg_loss + "/DM%s.nb" % chan_name
+    #     ofile_no_loss = open(ofname_no_loss, "w")
+    #     ofile_with_loss = open(ofname_with_loss, "w")
+    #     ofile_with_avg_loss = open(ofname_with_avg_loss, "w")
+    #     for dist in distrs:
+    #         if dist.energy_loss.lossType()==0:
+    #             ofile_no_loss.write(dist.printArray() + "\n")
+    #         elif dist.energy_loss.lossType()==1:
+    #             ofile_with_loss.write(dist.printArray() + "\n")
+    #         elif dist.energy_loss.lossType()==2:
+    #             ofile_with_avg_loss.write(dist.printArray() + "\n")
+    #     ofile_no_loss.close()
+    #     ofile_with_loss.close()
 
 
     def drawHist(h, color, linestyle):
@@ -312,32 +352,3 @@ if __name__=="__main__":
 
         plt.savefig("nuSpec_%s.pdf" % fname)
         plt.clf()
-
-#    plot("mass_1000/particle_24/energyLoss_hhad_(.)*_lhad_(.)*_chlep_(.)*/numu", "W_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating b/c hadron E loss", legendFormat=r"{neutrino_flavour} {bc_had_loss}")
-#
-#    plot(".*/particle_6/energyLoss_hhad_([0-9])_lhad_0_chlep_0/nutau", "top_hhad_nutau",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating b/c hadron E loss", legendFormat=r"{neutrino_flavour} {bc_had_loss}")
-#
-#    plot(".*/particle_6/energyLoss_hhad_([0-9])_lhad_0_chlep_0/nuel", "top_hhad_nuel",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating b/c hadron E loss", legendFormat=r"{neutrino_flavour} {bc_had_loss}")
-#
-#    plot(".*/particle_6/energyLoss_hhad_0_lhad_([0-9])_chlep_0/numu", "top_lhad_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating light hadron E loss", legendFormat=r"{neutrino_flavour}; {l_had_loss}")
-#
-#    plot(".*/particle_6/energyLoss_hhad_0_lhad_0_chlep_([0-9])/numu", "top_chlep_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating charged lepton E loss", legendFormat=r"{neutrino_flavour}; {ch_lep_loss}")
-#
-#    plot(".*/particle_15/energyLoss_hhad_([0-9])_lhad_0_chlep_0/numu", "tau_hhad_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating b/c hadron E loss", legendFormat=r"{neutrino_flavour}; {bc_had_loss}")
-#
-#    plot(".*/particle_15/energyLoss_hhad_0_lhad_([0-9])_chlep_0/numu", "tau_lhad_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating light hadron E loss", legendFormat=r"{neutrino_flavour}; {l_had_loss}")
-#
-#    plot(".*/particle_15/energyLoss_hhad_0_lhad_0_chlep_([0-9])/numu", "tau_chlep_numu",
-#            titleFormat=r"spectrum of $E(\nu)$ for DM({mass}) $\rightarrow$ {partname}, variating charged lepton E loss", legendFormat=r"{neutrino_flavour}; {ch_lep_loss}")
-#
-#    plot(".*/particle_([0-9])+/energyLoss_hhad_0_lhad_0_chlep_0/numu", "all_no_loss", titleFormat=r"spectrum of E({neutrino_flavour}) for DM({mass}), no energy loss", legendFormat=r"{partname}")
-#    plot("mass_1000/particle_([0-9])+/energyLoss_hhad_2_lhad_1_chlep_2/numu", "all_mc_loss", titleFormat=r"spectrum of E({neutrino_flavour}) for DM({mass}), MC energy loss", legendFormat=r"{partname}")
-#    plot(".*/particle_([0-9])+/energyLoss_hhad_1_lhad_1_chlep_1/numu", "all_avg_loss", titleFormat=r"spectrum of E({neutrino_flavour}) for DM({mass}), average energy loss", legendFormat=r"{partname}")
-#for (k, v) in energy_distributions.items():
