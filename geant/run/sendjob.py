612a0ab5694154a405b93b9a09e782e14b26ffd2
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-08-28 11:20:12 +0300

    Make `sendjob` use slurpy.

--- geant/run/sendjob.py
+++ geant/run/sendjob.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-import os,subprocess,argparse
+import os
+import os.path
+import subprocess
+import argparse
+import slur
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pid', type=int)
@@ -8,6 +12,7 @@ parser.add_argument('-n', '--jobs', type=int, default=1)
 parser.add_argument('-r', '--runs', type=int, default=1)
 parser.add_argument('-p', '--partition', type=str, default='main')
 parser.add_argument('--dry', action='store_const', const=True, default=False)
+parser.add_argument('--sbatch', action='store_const', const=True, default=False)
 args = parser.parse_args()
 print 'Arguments:', args
 
@@ -16,22 +21,29 @@ if not os.path.isfile('jobs/pythia.card'): os.symlink('../../src/scripts/pythia.
 os.chdir('jobs')
 
 # Build the sbatch command:
-cmd  = ['sbatch']
-cmd += ['-p{0}'.format(args.partition)]
-cmd += ['../batch.sh']
-cmd += ['-r{0}'.format(args.runs)]
-cmd += [str(args.pid), args.mass]
-print 'CMD list:', cmd
-print 'Command:', ' '.join(cmd)
+sbatch = 'sbatch'
+args_slurm = ['-p{0}'.format(args.partition)]
+script = '../batch.sh'
+args_script  = ['-r{0}'.format(args.runs)]
+args_script += [str(args.pid), args.mass]
 
-print 'Will submit {0} jobs.'.format(args.jobs)
-submitted = 0
-while submitted < args.jobs:
-	print 'Jobs submitted: {0} out of {1} ({2:.0f}%)'.format(submitted, args.jobs, 100*float(submitted)/float(args.jobs))
-	if args.dry:
-		print 'Dry run:', ' '.join(cmd)
-		submitted += 1
-	else:
-		if subprocess.call(cmd) == 0: submitted += 1
-		else: print 'Error submitting. Not increasing counter..'
-print 'All {0} jobs submitted done!'.format(args.jobs)
+if args.sbatch:
+	print 'Submitting directly!'
+	cmd = [sbatch]+args_slurm+[script]+args_script
+	print 'Command:', ' '.join(cmd)
+
+	print 'Will submit {0} jobs.'.format(args.jobs)
+	submitted = 0
+	while submitted < args.jobs:
+		print 'Jobs submitted: {0} out of {1} ({2:.0f}%)'.format(submitted, args.jobs, 100*float(submitted)/float(args.jobs))
+		if args.dry:
+			print 'Dry run:', ' '.join(cmd)
+			submitted += 1
+		else:
+			if subprocess.call(cmd) == 0: submitted += 1
+			else: print 'Error submitting. Not increasing counter..'
+else:
+	print 'Submitting to SLUR queue.'
+	slurdb=slur.SlurDB()
+	slurdb.queue_job(os.path.abspath(script), args_script, args_slurm, N=args.jobs)
+print 'All {0} jobs submitted. Done!'.format(args.jobs)
