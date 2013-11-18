f5b9d3cdefbe6478b2bba00cc97813fc008c7a67
Morten Piibeleht <morten.piibeleht@gmail.com>
2013-11-18 18:10:57 +0200

    Support M and G modifiers for mass strings.

--- geant/analyze/printsummaries.py
+++ geant/analyze/printsummaries.py
@@ -18,14 +18,23 @@ pnames = {
     1: 'down', 2: 'up', 3: 's', 4: 'c', 5: 'b', 6: 't',
 	11: 'e', 13: 'mu', 15: 'tau',
 	12: 'nue', 14: 'numu', 16: 'nutau',
-	21: 'g', 22: 'gamma', 23: 'Z', 24: 'W', 25: 'h'
+	21: 'g', 22: 'gamma', 23: 'Z', 24: 'W', 25: 'h',
+	321: 'K+'
 }
 
+def decode_mass(mstr):
+	if mstr[-1] == 'G':
+		return float(mstr[0:-1])
+	elif mstr[-1] == 'M':
+		return float(mstr[0:-1])/1.0e3
+	else:
+		return float(mstr)
+
 tfile = ROOT.TFile(args.fname)
-for mass in sorted(map(lambda k: int(k.GetName().split('_')[1]), tfile.GetListOfKeys())):
+for mass in sorted(map(lambda k: (k.GetName().split('_')[1]), tfile.GetListOfKeys())):
 	key_mass = tfile.FindKey('mass_{0}'.format(mass))
 	print
-	print 'DM particle mass: {0:5,.0f} GeV'.format(float(key_mass.GetName().split('_')[1]))
+	print 'DM particle mass: {0:11,.6f} GeV'.format(decode_mass(mass))
 	print 'channel     events '+nustrfmt.format(*nus)
 	for pid in sorted(map(lambda k: int(k.GetName().split('_')[1]), key_mass.ReadObj().GetListOfKeys())):
 		key_particle = key_mass.ReadObj().FindKey('particle_{0}'.format(pid))
